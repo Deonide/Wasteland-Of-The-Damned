e8d33c46660081f4367c553e99081b2f70ceccc7
Frans Erhart <franserhart12@gmail.com>
2025-04-04 01:49:45 +0200

    HollowWretch

---
+++ Assets/Scripts/Enemies/Maggothorn/HollowStrike.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class HollowStrike : MonoBehaviour
+{
+    [SerializeField]
+    private int m_damage;
+
+    private Rigidbody m_rb;
+
+    protected void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, 10f);
+    }
+
+    protected void FixedUpdate()
+    {
+        m_rb.AddForce(transform.forward * 10);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.ToString());
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+            player.m_currentHealth -= m_damage;
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
